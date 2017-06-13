631512c176361b2a2e1185cffe78af2a0b26f204
Ivan Samorodov <gsitex@gmail.com>
2017-06-13 23:40:23 +0600

    add CommentList to Article

--- src/Article.js
+++ src/Article.js
@@ -1,26 +1,44 @@
 import React, {Component} from 'react'
+import CommentList from './CommentList'
 
 export default class Article extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            isOpen: true
+            isOpen: true,
+            showComments: false
         }
     }
 
     render() {
         const {article} = this.props
-        const {isOpen} = this.state
         return (
             <div>
                 <h3>{article.title}</h3>
-                <button onClick = {this.toggleOpen}>
-                    {isOpen ? 'close' : 'open'}
-                </button>
+                {this.getButtons()}
                 {this.getBody()}
+                {this.getComments()}
             </div>
         )
+    }
+
+    getButtons() {
+        const {isOpen} = this.state
+        const {showComments} = this.state
+        const {article} = this.props
+
+        const commentButton =
+            <button onClick = {this.toggleComments}>
+                {showComments ? 'hide comments' : 'show comments'}
+            </button>
+
+        return <div>
+            <button onClick = {this.toggleOpen}>
+                {isOpen ? 'hide text' : 'show text'}
+            </button>
+            {article.comments ? commentButton : ''}
+        </div>
     }
 
     getBody() {
@@ -29,11 +47,26 @@ export default class Article extends Component {
         return <section>{article.text}</section>
     }
 
+    getComments() {
+        if (!this.state.showComments) return null
+        const {article} = this.props
+        if (article.comments) {
+            return <CommentList comments = {article.comments} />
+        }
+    }
+
     toggleOpen = (ev) => {
         ev.preventDefault()
         console.log('---', ev.nativeEvent)
         this.setState({
             isOpen: !this.state.isOpen
+        })
+    }
+
+    toggleComments = (ev) => {
+        ev.preventDefault()
+        this.setState({
+            showComments: !this.state.showComments
         })
     }
 }
---
+++ src/Comment.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Comment extends Component {
+    render() {
+        const {comment} = this.props
+        return (
+            <div>
+                <h4>{comment.user}</h4>
+                <p>{comment.text}</p>
+            </div>
+        )
+    }
+}
