c6150de059c3e0d641b6e568fb93c88618ae2e92
Ivan Samorodov <gsitex@gmail.com>
2017-06-20 20:08:01 +0600

    HT3.3 Добавить валидацию (подсвечивать красным инпут, если там < 5 или > 15 символов для имени и <20 или >50 для текста)

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -8,15 +8,27 @@ class CommentForm extends Component {
 
     state = {
         user: '',
+        user_error: false,
         text: '',
+        text_error: false,
     }
 
     render() {
+        const errorStyle = {color: 'red', fontWeight: 'bold'};
         return (
             <fieldset>
                 <legend>Add Comment:</legend>
-                <div>User: <input type = 'text' value = {this.state.user} onChange = {this.handleUserChange} /></div>
-                <div>Text: <textarea value={this.state.text} onChange = {this.handleTextChange} /></div>
+                <div>
+                    User: <input style = {this.state.user_error ? errorStyle : {}}
+                                 type = 'text'
+                                 value = {this.state.user}
+                                 onChange = {this.handleUserChange}/>
+                </div>
+                <div>
+                    Text: <textarea style = {this.state.text_error ? errorStyle : {}}
+                                    value = {this.state.text}
+                                    onChange = {this.handleTextChange} />
+                </div>
                 <div><button>submit</button></div>
             </fieldset>
         )
@@ -24,13 +36,15 @@ class CommentForm extends Component {
 
     handleUserChange = (ev) => {
         this.setState({
-            user: ev.target.value
+            user_error: (ev.target.value.length < 5 || ev.target.value.length > 15),
+            user: ev.target.value,
         })
     }
 
     handleTextChange = (ev) => {
         this.setState({
-            text: ev.target.value
+            text_error: (ev.target.value.length < 20 || ev.target.value.length > 50),
+            text: ev.target.value,
         })
     }
 
