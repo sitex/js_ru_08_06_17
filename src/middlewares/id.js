6b2993f1b4c80313fac36aaf5550d69364cda1a6
Roman <kaboom.ua@gmail.com>
2017-07-03 10:42:10 +0300

    Update id.js

--- src/middlewares/id.js
+++ src/middlewares/id.js
@@ -1,6 +1,7 @@
 import { CREATE_COMMENT } from '../constants'
 
 export default store => next => action => {
+	//через мидлвары будут проходить все экшины, суть в том, что делать их нужно максимально реюзабильными. Не завязывайся на один экшин
     if (action.type === CREATE_COMMENT) {
         action.payload.comment.id = String(Math.floor(Math.random() * 1000000))
     }
